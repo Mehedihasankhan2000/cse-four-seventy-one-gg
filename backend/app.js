3cae1a28eb0b444c176a42d0f2e8e739aba9e7e2
nimai <mehedi.hasan.khan@g.bracu.ac.bd>
2026-04-18 14:24:00 +0600

    added

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 app.listen(port, () => {console.log(`Server running on port ${port}`)})
